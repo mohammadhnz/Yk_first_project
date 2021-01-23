4a5e60a36dce23cb06211a0d7ab52fd521346b70
mohammadhnz <hmohammadali2013@gmail.com>
2021-01-23 13:52:03 +0330

    add: create models.

--- Yektanet/advertiser_mangement/models.py
+++ Yektanet/advertiser_mangement/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class BaseAdvertising(models.Model):
+    clicks = models.IntegerField(default= 0)
+    views = models.IntegerField(default= 0)
+
+    class Meta:
+        abstract = True
+
+
+class Advertiser(BaseAdvertising):
+    name = models.CharField(max_length=20)
+
+class Ad(BaseAdvertising):
+    title = models.CharField(max_length=20)
+    link = models.CharField(max_length=100)
+    imgUrl = models.CharField(max_length=100)
