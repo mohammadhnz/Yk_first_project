f50e27618c41036c2222b6662dba423b8a353125
mohammadhnz <hmohammadali2013@gmail.com>
2021-01-28 12:19:57 +0330

    feature: create urls.py in advertiser_mangement,add advertiser_mangement to settings.py.

--- Yektanet/advertiser_mangement/views.py
+++ Yektanet/advertiser_mangement/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+
+def advertiser_mangement1(request):
+    return HttpResponse("Thie is advertiser_management")
+
+
+def show_message(request):
+    return JsonResponse({"name": "محمدعلی"})
