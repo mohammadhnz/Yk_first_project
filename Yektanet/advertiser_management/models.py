244bd605c63e788e4212ead9b37f89696b0eb6e1
mohammadhnz <hmohammadali2013@gmail.com>
2021-02-02 13:59:54 +0330

    refactor: change models and migrate

--- Yektanet/advertiser_management/models.py
+++ Yektanet/advertiser_management/models.py
@@ -41,11 +41,18 @@ class Advertiser(BaseAdvertising):
 
 
 class Ad(models.Model):
+    APPROVE_CHOICES = (
+        ('a','APPROVED'),
+        ('d','Disapproved'),
+        ('n','Not checked')
+    )
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=20)
     link = models.CharField(max_length=100)
     image = models.ImageField(upload_to="images", default="")
     advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
+    approve = models.CharField(max_length=1,choices=APPROVE_CHOICES,default=APPROVE_CHOICES[2])
+
 
     def __str__(self):
         return str(self.id)
